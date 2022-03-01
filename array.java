f6e23a6d9d809f3b7ecb031d8325e991a78d720e
sahu111 <pks.iiit@gmail.com>
2022-03-02 01:05:44 +0530

    Array Introduction

---
+++ array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+    public class array{
+
+    public static void main(String[] args) {
+     // array of size 5
+    int[] arr = new int[5];
+    
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
+    arr[4] = 50;
+
+    for(int i = 0; i<arr.length; i++){
+        System.out.println(arr[i]);
+    }
+
+    }
+
+    }
+    
